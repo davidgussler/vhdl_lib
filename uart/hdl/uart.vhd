8891d2dc6c196fda3df36125b0f8afcc6d777e6b
davidgussler <davidnguss@gmail.com>
2023-04-16 19:18:26 -0400

    have the general structure of the uard module done. But I was very tired and lazy when coding. need to go thru and double check stuff before I start testing.

--- uart/hdl/uart.vhd
+++ uart/hdl/uart.vhd
@@ -32,6 +32,7 @@
 -- #  POSSIBILITY OF SUCH DAMAGE.
 -- # ===========================================================================
 -- # 
+-- # Source: https://forum.digikey.com/t/uart-vhdl/12670
 -- # 
 -- #############################################################################
 
@@ -46,6 +47,8 @@ entity uart is
         G_CLK_FREQ_HZ : positive              := 125000000; 
         -- Baud rate of the UART. Typical values are 115200 or 9600
         G_BAUD_RATE   : positive              := 115200;
+        -- Samples to rx per baud period
+        G_OS_RATE     : positive              := 16; 
         -- Number of data bits per UART transfer
         G_DATA_WIDTH   : positive range 5 to 8 := 8;
         -- 1 = use parity 
@@ -76,7 +79,8 @@ entity uart is
 
         -- Status
         o_parity_err : out std_logic;
-        o_frame_err  : out std_logic
+        o_frame_err  : out std_logic;
+        o_missed_rx_err : out std_logic
     );
 end entity;
 
@@ -85,45 +89,67 @@ architecture rtl of uart is
     -- Calculate number of clocks per baud based off of the user-given clock
     -- frequency and baud rate
     constant CLKS_PER_BAUD : positive := G_CLK_FREQ_HZ / G_BAUD_RATE; 
+    constant CLKS_PER_OS : positive := CLKS_PER_BAUD / G_OS_RATE;
 
     -- Data + start + stop + parity 
-    constant BUF_WIDTH : positive := G_DATA_WIDTH + G_PARITY + 2; 
+    constant BUF_WIDTH : positive := G_DATA_WIDTH + G_PARITY + 2;
 
-    type uart_state_t is (S_IDLE, S_DATA, S_BUF);
-    signal tx_state     : uart_state_t;
-    signal rx_state     : uart_state_t;
-
+    type tx_state_t is (S_TX_IDLE, S_TX_BUF, S_TX_DATA);
+    signal tx_state     : tx_state_t;
     signal tx_data : std_logic_vector(G_DATA_WIDTH-1 downto 0); 
     signal tx_buf : std_logic_vector(BUF_WIDTH-1 downto 0); 
     signal tx_baud_pulse : std_logic; 
-    signal tx_baud_cnt : unsigned(clog2(CLKS_PER_BAUD)-1 downto 0); 
-    signal tx_buf_idx : unsigned(clog2(BUF_WIDTH)-1 downto 0); 
+    signal tx_baud_clk_cnt : integer range CLKS_PER_BAUD-1 downto 0; 
+    signal tx_buf_idx : integer range BUF_WIDTH-1 downto 0; 
     signal tx_parity : std_logic_vector(G_DATA_WIDTH downto 0); 
 
+    type rx_state_t is (S_RX_IDLE, S_RX_START, S_RX_DATA);
+    signal rx_state     : rx_state_t;
+    signal rx_os_pulse : std_logic; 
+    signal rx_os_pulse_cnt : integer range G_OS_RATE-1 downto 0;
+    signal rx_os_clk_cnt : integer range CLKS_PER_OS-1 downto 0; 
+    signal rx_data_idx : integer range G_DATA_WIDTH-1 downto 0; 
+    signal rx_parity : std_logic_vector(G_DATA_WIDTH downto 0); 
+    signal rx_buf : std_logic_vector(BUF_WIDTH-1 downto 0);
+    signal rx_buf_idx : integer range BUF_WIDTH-1 downto 0; 
+    signal new_rx_dat_pulse : std_logic; 
 
 begin
 
-    -- -------------------------------------------------------------------------
-    -- UART Tx 
-    -- -------------------------------------------------------------------------
-    -- TX Baud Rate Counter
+    -- Baud rate and oversampling counters
     process (i_clk)
     begin
         if rising_edge(i_clk) then
             if (i_rst) then
-                tx_baud_cnt <= (others=>'0');
+                tx_baud_clk_cnt <= 0;
                 tx_baud_pulse <= '0';
+                rx_os_clk_cnt <= 0;
+                rx_os_pulse <= '0';
             else
-                if (tx_baud_cnt < CLKS_PER_BAUD) then 
-                    tx_baud_cnt <= tx_baud_cnt + 1; 
+                if (tx_baud_clk_cnt < CLKS_PER_BAUD-1) then 
+                    tx_baud_clk_cnt <= tx_baud_clk_cnt + 1; 
                     tx_baud_pulse <= '0';
                 else 
-                    tx_baud_cnt <= (others=>'0');
+                    rx_os_clk_cnt <= 0;
+                    tx_baud_clk_cnt <= 0;
                     tx_baud_pulse <= '1';
+                end if;
+
+                if (rx_os_clk_cnt < CLKS_PER_OS-1) then 
+                    rx_os_clk_cnt <= rx_os_clk_cnt + 1;
+                    rx_os_pulse <= '0';
+                else 
+                    rx_os_clk_cnt <= 0;
+                    rx_os_pulse <= '1';
                 end if;
             end if;
         end if;
     end process;
+
+
+    -- -------------------------------------------------------------------------
+    -- UART Tx 
+    -- -------------------------------------------------------------------------
 
     -- Tx parity calculation
     -- 9 inputs => two levels of LUTs
@@ -137,39 +163,39 @@ begin
     begin
         if rising_edge(i_clk) then
             if (i_rst) then
-                tx_state        <= S_IDLE;
+                tx_state        <= S_TX_IDLE;
                 o_s_axis_tready <= '1';
                 o_uart_tx       <= '1'; 
                 tx_buf <= (others=>'0'); -- TODO: doesnt need a reset 
                 tx_data <= (others=>'0'); -- TODO: doesnt need a reset 
-                tx_buf_idx <= (others=>'0'); 
+                tx_buf_idx <= 0; 
             else
                 case (tx_state) is
-                when S_IDLE =>
+                when S_TX_IDLE =>
                     -- Register the data
                     if (i_s_axis_tvalid and o_s_axis_tready) then
-                        tx_state <= S_BUF; 
+                        tx_state <= S_TX_BUF; 
                         o_s_axis_tready <= '0'; 
                         tx_data <= i_s_axis_tdata; 
                     else 
                         o_s_axis_tready <= '1';
                     end if; 
 
-                when S_BUF =>
+                when S_TX_BUF =>
                     -- add start / stop / parity to tx buffer
-                    tx_state <= S_DATA; 
+                    tx_state <= S_TX_DATA; 
                     if (G_PARITY = 0) then  
                         tx_buf <= '1' & tx_data & '0'; 
                     else 
                         tx_buf <= '1' & tx_parity(G_DATA_WIDTH) & tx_data & '0'; 
                     end if; 
 
-                when S_DATA =>
-                    if (tx_buf_idx >= BUF_WIDTH) then 
-                        tx_state <= S_IDLE; 
-                        tx_buf_idx <= (others=>'0');
+                when S_TX_DATA =>
+                    if (tx_buf_idx >= BUF_WIDTH-1) then 
+                        tx_state <= S_TX_IDLE; 
+                        tx_buf_idx <= 0;
                     elsif (tx_baud_pulse) then
-                        o_uart_tx  <= tx_buf(to_integer(tx_buf_idx)); 
+                        o_uart_tx  <= tx_buf(tx_buf_idx); 
                         tx_buf_idx <= tx_buf_idx + 1;
                     end if; 
 
@@ -184,17 +210,109 @@ begin
 
 
 
-
-
     -- UART Rx -----------------------------------------------------------------
     -- -------------------------------------------------------------------------
 
+    rx_parity(0) <= G_PARITY_EO;
+    rx_parity_logic: for i in 0 to G_DATA_WIDTH-1 generate
+        rx_parity(i+1) <= rx_parity(i) xor rx_buf(i);
+    end generate;
+                      
+    -- RX AXIS and error logic 
+    process (i_clk)
+    begin
+        if rising_edge(i_clk) then
+            if (i_rst) then
+                o_m_axis_tvalid <= '0';
+                o_m_axis_tdata <= (others=>'0'); -- TODO: does not need to be reset
+                o_parity_err <= '0'; 
+                o_frame_err <= '0'; 
+                o_missed_rx_err <= '0'; 
+            else
+                o_parity_err <= '0'; 
+                o_frame_err <= '0'; 
+                o_missed_rx_err <= '0'; 
+
+                if (new_rx_dat_pulse) then
+                    if (o_m_axis_tvalid) then 
+                        -- We missed data becasue the receiver fsm got new data 
+                        -- before the axi channel had received the valid last data.
+                        o_missed_rx_err <= '1'; 
+                    else 
+                        o_m_axis_tvalid <= '1';
+                        o_m_axis_tdata <= rx_buf(G_DATA_WIDTH-1 downto 1);
+
+                        o_frame_err <= rx_buf(0) or not rx_buf(BUF_WIDTH-1);
+
+                        if (G_PARITY = 0) then 
+                            o_parity_err <= '0'; 
+                        else 
+                            o_parity_err <= rx_parity(G_DATA_WIDTH) xor rx_buf(G_DATA_WIDTH);
+                        end if; 
+                    end if; 
+                end if; 
+
+                if (o_m_axis_tvalid and i_m_axis_tready) then
+                    o_m_axis_tvalid <= '0';
+                end if; 
+            end if;
+        end if;
+    end process;
+
+
     -- RX Controller
+    process (i_clk)
+    begin
+        if rising_edge(i_clk) then
+            if (i_rst) then
+                rx_state        <= S_RX_IDLE;
+                rx_buf <= (others=>'0'); -- TODO: does not need to be reset
+                rx_buf_idx <= 0;
+                new_rx_dat_pulse <= '0';
+            else
+                new_rx_dat_pulse <= '0';
 
+                if (rx_os_pulse) then 
 
-    -- RX Data Bit Counter
+                    case (rx_state) is
+                    when S_RX_IDLE =>
+                        rx_os_pulse_cnt <= 0;
+                        rx_buf_idx <= 0;
+                        if (i_uart_rx = '0') then -- begenning of start bit
+                            rx_state <= S_RX_START; 
+                        end if; 
 
+                    when S_RX_START =>
+                        if (rx_os_pulse_cnt < G_OS_RATE/2-1) then -- minus 1 since we alread got one in the idle state
+                            rx_os_pulse_cnt <= rx_os_pulse_cnt + 1; 
+                        else 
+                            -- half-way thru start bit 
+                            rx_buf(rx_buf_idx) <= i_uart_rx; 
+                            rx_buf_idx <= rx_buf_idx + 1; 
+                            rx_state <= S_RX_DATA; 
+                            rx_os_pulse_cnt <= 0; 
+                        end if; 
 
-    
+                    when S_RX_DATA =>
+                        if (rx_os_pulse_cnt < G_OS_RATE-1) then 
+                            rx_os_pulse_cnt <= rx_os_pulse_cnt + 1; 
+                        else 
+                            rx_os_pulse_cnt <= 0; 
+                            if (rx_buf_idx < BUF_WIDTH) then 
+                                rx_buf(rx_buf_idx) <= i_uart_rx; 
+                                rx_buf_idx <= rx_buf_idx + 1; 
+                            else 
+                                new_rx_dat_pulse <= '1';
+                                rx_buf_idx <= 0; 
+                                rx_state <= S_RX_IDLE;
+                            end if; 
+                        end if; 
+                    when others =>
+                        null;
+                    end case;
+                end if; 
+            end if;
+        end if;
+    end process;
 
 end architecture;
